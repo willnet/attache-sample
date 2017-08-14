f92571a577c840d14c58b6f294d65786ec70a5fb
willnet <netwillnet@gmail.com>
2017-08-14 17:55:47 +0900

    Add dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require bootstrap/dist/js/bootstrap
+//= require attache
 //= require_tree .
